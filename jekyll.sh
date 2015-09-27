7f8089793f8af4e6395b97e5583e3ca3a46b7368
J Harper <jharper@insidesecure.com>
2015-09-26 19:58:48 -0700

    u

---
+++ jekyll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# @see https://help.github.com/articles/using-jekyll-with-pages/
+# gem install bundler
+# echo "source 'https://rubygems.org'" > Gemfile
+# echo "gem 'github-pages'" >> Gemfile
+# bundle install
+bundle exec jekyll serve
