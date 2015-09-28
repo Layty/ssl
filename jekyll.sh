6058f9ce7546443bf0d373af19920790ecef72f3
J Harper <jharper@insidesecure.com>
2015-09-28 14:35:26 -0700

    u

--- jekyll.sh
+++ jekyll.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # @see https://help.github.com/articles/using-jekyll-with-pages/
-# gem install bundler
+# sudo gem install bundler
 # echo "source 'https://rubygems.org'" > Gemfile
 # echo "gem 'github-pages'" >> Gemfile
 # bundle install
+# @note if that fails on OS X, install command line tools: xcode-select --install
 bundle exec jekyll serve
